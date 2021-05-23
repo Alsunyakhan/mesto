8b8f1650a1c4f81dc56724a87b652f3cadf30a3c
Alsu <Alsunya@yandex.ru>
2021-05-24 00:37:08 +0500

    mesto1

---
+++ main.js
@@ -0,0 +1,30 @@
+let openPopupButton = document.querySelector('.popup_open');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close');
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status')
+let formElement = document.querySelector('.popup__container-form');
+let nameInput = document.querySelector('#edit-name');
+let statusInput = document.querySelector('#edit-status');
+
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    statusInput.value = profileStatus.textContent;
+};
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+};
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileStatus.textContent = statusInput.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
