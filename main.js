29d29eba02bc0ac1c16cf151f292024dae5c85d2
Alsu <Alsunya@yandex.ru>
2021-05-24 09:43:49 +0500

    mestoN3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let openPopupButton = document.querySelector('.popup_open');
+let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
